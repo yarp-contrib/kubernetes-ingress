e875014555be264b8c840f4a73ad93459d1f59fe
Joao Correia <joaope@gmail.com>
2025-09-26 23:31:41 +0100

    join monitor and controller one repo

---
+++ src/YarpContrib.Kubernetes.Ingress.Monitor/Program.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+using var serilog = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(serilog, dispose: false);
+
+builder.Configuration.AddEnvironmentVariables();
+builder.Configuration.AddCommandLine(args, new Dictionary<string, string>
+{
+    { "--controller-class", "Yarp.ControllerClass" },
+    { "-c", "Yarp.ControllerClass" },
+    { "--controller-service-name", "Yarp.ControllerServiceName" },
+    { "-s", "Yarp.ControllerServiceName" },
+    { "--controller-service-namespace", "Yarp.ControllerServiceNamespace" },
+    { "-n", "Yarp.ControllerServiceNamespace" }
+});
+
+builder.Services
+    .AddHealthChecks()
+    .Services
+    .AddKubernetesIngressMonitor(builder.Configuration)
+    .AddControllers()
+    .AddKubernetesDispatchController();
+
+var app = builder.Build();
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+{
+    Predicate = _ => false
+});
+
+app.MapHealthChecks("/health/ready", new HealthCheckOptions
+{
+    Predicate = _ => true
+});
+
+app.Run();
