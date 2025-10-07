e92767edfc1fc524762c39d5e27fbf0dd14a6906
Joao Correia <joaope@gmail.com>
2025-10-07 03:14:37 +0100

    remove usings

--- src/YarpContrib.Kubernetes.Ingress.Controller/Program.cs
+++ src/YarpContrib.Kubernetes.Ingress.Controller/Program.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
--- src/YarpContrib.Kubernetes.Ingress.Monitor/Program.cs
+++ src/YarpContrib.Kubernetes.Ingress.Monitor/Program.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
