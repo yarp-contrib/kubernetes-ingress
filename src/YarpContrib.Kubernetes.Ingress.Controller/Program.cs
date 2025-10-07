a93d3d06d33ddd43dbfbe21495e6b25bab15b960
Joao Correia <joaope@gmail.com>
2025-10-07 02:08:13 +0100

    standalone

--- src/YarpContrib.Kubernetes.Ingress.Controller/Program.cs
+++ src/YarpContrib.Kubernetes.Ingress.Controller/Program.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,7 +7,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
-using Yarp.Kubernetes.Controller;
 using Yarp.Kubernetes.Protocol;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -35,26 +32,15 @@ builder.Configuration.AddCommandLine(args, new Dictionary<string, string>
     { "-m", "ControllerUrl" }
 });
 
-    var options = new ReceiverOptions();
-    builder.Configuration.Bind(options);
-
-    var yarp = new YarpOptions();
-    builder.Configuration.GetSection("Yarp").Bind(yarp);
-
-    Console.WriteLine("Starting args " + string.Join(' ', args));
-    Console.WriteLine("Starting config " + options.ControllerUrl + " - " + builder.Configuration["ControllerUrl"] + " - " + JsonSerializer.Serialize(yarp));
-
 var isStandalone = string.IsNullOrEmpty(builder.Configuration["ControllerUrl"]);
 
 if (isStandalone)
 {
-    builder.Services
-        .AddKubernetesReverseProxy(builder.Configuration);
+    builder.WebHost.UseKubernetesReverseProxyCertificateSelector();
+    builder.Services.AddKubernetesReverseProxy(builder.Configuration);
 }
 else
 {
-    builder.WebHost.UseKubernetesReverseProxyCertificateSelector();
-    
     builder.Services
         .Configure<ReceiverOptions>(builder.Configuration.Bind)
         .AddHostedService<Receiver>()
@@ -64,13 +50,7 @@ else
 
 var app = builder.Build();
 
-app.Map("/health/live", async c => {
-    var options = new ReceiverOptions();
-    builder.Configuration.Bind(options);
-    var logger = c.RequestServices.GetRequiredService<ILogger<Program>>();
-    logger.LogInformation("Liveness check, controller url: {IsStandalone} {ControllerUrl}", isStandalone, options.ControllerUrl);
-    await c.Response.WriteAsync("Alive");
-});
+app.Map("/health/live", async c => await c.Response.WriteAsync("Alive"));
 app.Map("/health/ready", async c => await c.Response.WriteAsync("Ready"));
 
 app.MapReverseProxy();
